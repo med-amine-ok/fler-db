a24139a18187ff0654633401b1f32b3119227a90
Your Name <your-email@example.com>
2026-01-16 20:37:56 +0100

    kawav ro7k zadt yacine

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,5 +8,6 @@ export const ALLOWED_EMAILS = [
     'yanir_iskandar.louni@g.enp.edu.dz',
     'medamineouldkhaoua@gmail.com',
     'abdallah.mohellebi@g.enp.edu.dz',
-    'maroua.laras@g.enp.edu.dz'
+    'maroua.laras@g.enp.edu.dz',
+    'ahmed_yassine.aliat@g.enp.edu.dz'
 ].map(email => email.toLowerCase().trim());
